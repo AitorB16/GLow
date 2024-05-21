5e765fa53d94c1dced850445fd472a67fd18e2c9
AitorB16 <belengueraitor@gmail.com>
2024-05-21 12:21:02 +0200

    Checkpoint individual executions with hydra tracker -> detailed output

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import flwr as fl
 import pickle
 from pathlib import Path
@@ -25,21 +24,14 @@ from flwr.server.client_manager import ClientManager, SimpleClientManager
 from custom_strategies.topology_based_GL import topology_based_Avg
 
 
-#@hydra.main(config_path="conf", config_name="base", version_base=None)
-def main():
+@hydra.main(config_path="conf", config_name="base", version_base=None)
+def main(cfg: DictConfig):
     #1. LOAD CONFIGURATION
-    #print(OmegaConf.to_yaml(cfg))
-    save_path = './outputs'
+    print(OmegaConf.to_yaml(cfg))
+    save_path = HydraConfig.get().runtime.output_dir
 
     #LOAD TOPOLOGY
-    conf_file = sys.argv[1]
-    run_id = sys.argv[2]
-    tplgy_file = sys.argv[3]
-
-    with open(conf_file, 'r') as file:
-        cfg = yaml.safe_load(file)
-
-    with open(tplgy_file, 'r') as file:
+    with open(cfg.topology, 'r') as file:
         tplgy = yaml.safe_load(file)
 
     num_clients = tplgy['num_clients']
@@ -51,10 +43,10 @@ def main():
 
     
     #2. PREAPRE YOUR DATASET
-    trainloaders, validationloaders, testloader = prepare_dataset(num_clients, tplgy['clients_with_no_data'], tplgy['last_connected_client'], cfg['batch_size'], cfg['seed'] )
+    trainloaders, validationloaders, testloader = prepare_dataset(num_clients, tplgy['clients_with_no_data'], tplgy['last_connected_client'], cfg.batch_size, cfg.seed, )
 
     #3. DEFINE YOUR CLIENTS
-    client_fn = generate_client_fn(vcid, trainloaders, validationloaders, cfg['num_classes'])
+    client_fn = generate_client_fn(vcid, trainloaders, validationloaders, cfg.num_classes)
 
 
     #4. DEFINE STRATEGY
@@ -63,12 +55,11 @@ def main():
         fraction_fit=0.00001,
         fraction_evaluate=0.00001,
         min_available_clients=num_clients,
-        on_fit_config_fn=get_on_fit_config(cfg['config_fit']),
-        evaluate_fn=get_evaluate_fn(cfg['num_classes'], testloader),
+        on_fit_config_fn=get_on_fit_config(cfg.config_fit),
+        evaluate_fn=get_evaluate_fn(cfg.num_classes, testloader),
         fit_metrics_aggregation_fn = cli_val_distr,
         evaluate_metrics_aggregation_fn = cli_eval_distr_results, #LOCAL METRICS CLIENT
-        total_rounds = cfg['num_rounds'],
-        run_id = run_id,
+        total_rounds = cfg.num_rounds,
         save_path = save_path
     )
 
@@ -85,7 +76,7 @@ def main():
     #for cli_ID in vcid:
     #    server_pool.append(fl.server.Server(client_manager = SimpleClientManager(), strategy = strategy))
 
-    server_config = fl.server.ServerConfig(num_rounds=cfg['num_rounds'])
+    server_config = fl.server.ServerConfig(num_rounds=cfg.num_rounds)
     server = fl.server.Server(client_manager = SimpleClientManager(), strategy = strategy)
 
 
@@ -100,7 +91,7 @@ def main():
     )
 
     #6. SAVE RESULTS
-    results_path = save_path + "/" + run_id + "_results.pkl"
+    results_path = Path(save_path) / "results.pkl"
 
     results = {"history": history, "anythingelse": "here"}
     
@@ -121,7 +112,7 @@ def main():
     out1 = "**losses_distributed: " + ' '.join([str(elem) for elem in history.losses_distributed]) + "\n\n**losses_centralized: " + ' '.join([str(elem) for elem in history.losses_centralized])
     out2 = out1 + '\n\n**acc_distr: ' + ' '.join([str(elem) for elem in history.metrics_distributed['acc_distr']]) + '\n\n**cid: ' + ' '.join([str(elem) for elem in history.metrics_distributed['cid']])
     out3 = out2 + '\n\n**metrics_centralized: ' + ' '.join([str(elem) for elem in history.metrics_centralized['acc_cntrl']]) + '\n'
-    f = open(save_path +"/" + run_id + "_raw_output.out", "w")
+    f = open(save_path + "/raw_output.out", "w")
     f.write(out3)
     f.close()
 
